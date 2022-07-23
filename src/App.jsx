0fc91e6b5dfde6cb499b75c8e5a13b721920888d
eva-hs <eva.holmback@gmail.com>
2022-07-23 18:49:07 +0200

    Implemented handle event for pagination

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Pagination from "./components/common/Pagination";
 class App extends Component {
   state = {
     foods: getFoods(),
+    isActive: [
+      { _id: 0, active: false },
+      { _id: 1, active: false },
+      { _id: 2, active: false },
+    ],
   };
 
   handleDelete = (_id) => {
@@ -19,6 +24,23 @@ class App extends Component {
     foods[index] = { ...food };
     foods[index].isFavorite = !foods[index].isFavorite;
     this.setState({ foods });
+  };
+
+  handlePaginationClick = (a) => {
+    // create new isActive array
+    const isActive = [
+      { _id: 0, active: false },
+      { _id: 1, active: false },
+      { _id: 2, active: false },
+    ];
+    // Find index place in old array
+    const index = this.state.isActive.indexOf(a);
+    // clone the object on the index place
+    isActive[index] = { ...a };
+    // Make changes
+    isActive[index].active = true;
+    //setState
+    this.setState({ isActive });
   };
 
   render() {
@@ -61,7 +83,10 @@ class App extends Component {
             ))}
           </tbody>
         </table>
-        <Pagination />
+        <Pagination
+          isActive={this.state.isActive}
+          onPaginationClick={this.handlePaginationClick}
+        />
       </>
     );
   }
--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,25 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Pagination(props) {
-  return (
-    <ul class="pagination">
-      <li class="page-item">
-        <a class="page-link" href="#">
-          1
-        </a>
-      </li>
-      <li class="page-item">
-        <a class="page-link" href="#">
-          2
-        </a>
-      </li>
-      <li class="page-item">
-        <a class="page-link" href="#">
-          3
-        </a>
-      </li>
-    </ul>
-  );
+class Pagination extends Component {
+  render() {
+    const { isActive, onPaginationClick } = this.props;
+
+    return (
+      <ul className="pagination">
+        <li className={this.formatPageItem(isActive[0].active)}>
+          <a
+            onClick={() => onPaginationClick(isActive[0])}
+            className="page-link"
+            href="#"
+          >
+            1
+          </a>
+        </li>
+        <li className={this.formatPageItem(isActive[1].active)}>
+          <a
+            onClick={() => onPaginationClick(isActive[1])}
+            className="page-link"
+            href="#"
+          >
+            2
+          </a>
+        </li>
+        <li className={this.formatPageItem(isActive[2].active)}>
+          <a
+            onClick={() => onPaginationClick(isActive[2])}
+            className="page-link"
+            href="#"
+          >
+            3
+          </a>
+        </li>
+      </ul>
+    );
+  }
+
+  // Formats the pagination depending on isActives value.
+  formatPageItem(isActive) {
+    let formatPageItem = "page-item ";
+    formatPageItem += isActive === true ? "active" : "disable";
+    return formatPageItem;
+  }
 }
 
 export default Pagination;
