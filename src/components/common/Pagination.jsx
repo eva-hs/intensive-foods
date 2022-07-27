0756b60985a2d36c566e1d8b4d2f2e2f4608fd93
eva-hs <eva.holmback@gmail.com>
2022-07-27 19:35:05 +0200

    installed and started to use Lodash in Pagination

--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,13 +1,13 @@
 import React from "react";
+import _ from "lodash";
 
 function Pagination({ itemCount, pageSize, selectedPage, onPageChange }) {
   const pageCount = Math.ceil(itemCount / pageSize);
+  console.log(pageCount);
 
   if (pageCount === 1) return null;
 
-  const pages = range(1, pageCount);
-
-  console.log(selectedPage);
+  const pages = _.range(1, pageCount + 1);
 
   return (
     <ul className="pagination">
@@ -27,8 +27,8 @@ function Pagination({ itemCount, pageSize, selectedPage, onPageChange }) {
 
 export default Pagination;
 
-function range(start, end) {
-  let result = [];
-  for (let i = start; i <= end; i++) result.push(i);
-  return result;
-}
+// function range(start, end) {
+//   let result = [];
+//   for (let i = start; i <= end; i++) result.push(i);
+//   return result;
+// }
