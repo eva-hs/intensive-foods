90a7d14b07f3a04ada27d0a7d211091c4a9e9d7f
eva-hs <eva.holmback@gmail.com>
2022-08-12 21:47:42 +0200

    Added form in FoodForm

--- src/components/FoodForm.jsx
+++ src/components/FoodForm.jsx
@@ -1,18 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
+import Joi from "joi";
+import Form from "./common/Form";
 
-class FoodForm extends Component {
-  handleSave = () => {
+class FoodForm extends Form {
+  state = {
+    data: {
+      name: "",
+      category: "",
+      numberInStock: "",
+      price: "",
+    },
+    errors: { name: "", category: "", numberInStock: "", price: "" },
+  };
+
+  schema = Joi.object({
+    name: Joi.string().required().label("Name"),
+    category: Joi.string().required().label("Category"),
+    numberInStock: Joi.number().required().label("Number in stock"),
+    price: Joi.number().required().label("Price"),
+  });
+
+  doSubmit = () => {
     this.props.history.push("/intensive-foods/foods");
   };
 
   render() {
     return (
-      <>
+      <form className="m-3" onSubmit={this.handleSubmit}>
         <h1>Food Form {this.props.match.params.id}</h1>
-        <button className="btn btn-primary" onClick={() => this.handleSave()}>
-          Save
-        </button>
-      </>
+        {this.renderInput("name", "Name")}
+        {this.renderInput("category", "Category")}
+        {this.renderInput("numberInStock", "Number in stock")}
+        {this.renderInput("price", "Price")}
+        {this.renderButton("Save")}
+      </form>
     );
   }
 }
